9ce52ee148f90f3330b2d75f2c37fbcb47344346
MaiBui-code <83102564+MaiBui-code@users.noreply.github.com>
2022-05-11 04:29:04 -0400

    OOP Start up: Ice Cream Object

---
+++ main.py
@@ -0,0 +1,25 @@
+from IceCreamScoop import IceCreamScoop
+
+def main():
+
+   vanillaScoop:IceCreamScoop = IceCreamScoop( "vanilla" )
+
+   vanillaScoop.addTopping( "sprinkles" )
+
+   print( vanillaScoop )
+
+
+
+   strawberryScoop:IceCreamScoop = IceCreamScoop( "strawberry" )
+
+   strawberryScoop.melt()
+
+   strawberryScoop.addTopping( "whipped cream" )
+
+   print( strawberryScoop )
+
+
+
+if __name__ == "__main__":
+
+   main()
